dc7730f43d43dfe3b2edad03552c7be193dc9c4c
ashimsharma10 <073bct508.ashim@pcampus.edu.np>
2021-03-02 22:43:41 +0545

    added utils package for R2B network

---
+++ R2B_Network/utils/losses.py
@@ -0,0 +1,14 @@
+import torch
+
+device = torch.device('cuda')
+
+cri_bce = torch.nn.BCEWithLogitsLoss().to(device)
+cri_fea = torch.nn.L1Loss().to(device)
+cri_pix = torch.nn.L1Loss().to(device)
+
+def cri_gan(inp, tf):
+    op = torch.empty_like(inp).fill_(0.0)
+    if tf:
+        op = torch.empty_like(inp).fill_(1.0)      
+    
+    return cri_bce(inp, op)
